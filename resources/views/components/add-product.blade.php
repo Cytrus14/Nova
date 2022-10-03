fd738787df9addf3ac7dd0c20ba8d9b427efac70
Dominik <dominik@dominik.com>
2022-10-03 09:37:53 +0200

    Input work in progress

---
+++ resources/views/components/add-product.blade.php
@@ -0,0 +1,16 @@
+<div class="flex justify-center bg-white">
+    <div class="mb-3 w-1/3">
+        <form>
+            <label for="productNameInput" class="form-label block mb-3 text-gray-800">
+                Product name
+            </label>
+            <input 
+            type="text"
+            class="form-control block rounded border border-solid border-gray-400"
+            id="productNameInput"
+            placeholder="Example product name"
+            >
+        </input>
+        </form>
+    </div>
+</div>
--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -12,7 +12,10 @@
             @endforeach
         </table>
     </div>
-    <x-product-card
+    <div class="flex justify-center">
+        <ul>
+            <li class="px-6 py-2">
+            <x-product-card
         name="testName"
         price="100.00"
         :rating="10"
@@ -21,4 +24,23 @@
         >
 
     </x-product-card>
+            </li>
+            <li>
+            <x-product-card
+        name="testName"
+        price="100.00"
+        :rating="10"
+        :isInStock="false"
+        description="test"
+        >
+        
+    </x-product-card>
+        </li>
+
+        </ul>
+    </div>
+    <x-paginated-product-list>
+        test123
+    </x-paginated-product-list>
+    <x-add-product></x-add-product>
 </body>
